0b9c31c8841c62b802b8c2614b37f3cc9a91ca78
anozik832 <anozik832@fhs.win.fuhsd.org>
2016-11-04 11:02:37 -0700

    improved algorithm

--- src/BasicPlotting.java
+++ src/BasicPlotting.java
@@ -1,42 +1,43 @@
-import java.util.Arrays;
-import java.util.Random;
 import javax.swing.JFrame;
+
 import org.math.plot.Plot2DPanel;
 
 public class BasicPlotting {
-	public static double[][] sampleData;
-	public static String datafile = "data/64StepsInPocketJogging-out.csv";
-	
-	public static void main(String[] args) {
-		// Create data set
-		CSVData dataset = CSVData.createDataSet(datafile, 0);
+  public static double[][] sampleData;
+  public static String datafile = "data/64StepsInPocketJogging-out.csv";
 
-		// Get 2d array of all data
-		sampleData = dataset.getAllData();
+  public static void main(String[] args) {
+    // Create data set
+    CSVData dataset = CSVData.createDataSet(datafile, 0);
 
-		double[] time = ArrayHelper.extractColumn(sampleData, 0);		
-		double[][] sensorData = ArrayHelper.extractColumns(sampleData, new int[] { 1, 2, 3, 4, 5, 6 });
-		
-		int steps = CountStepsBlank.countSteps(time, sensorData);
-		System.out.println(steps);
-		
-		double[][] accel = ArrayHelper.extractColumns(sampleData, new int[] { 1, 2, 3 });
-		double[] mags = CountStepsBlank.calculateMagnitudesFor(accel);
-		
-		System.out.println(CountStepsBlank.calculateMean(mags));
-		System.out.println(CountStepsBlank.calculateStandardDeviation(mags, CountStepsBlank.calculateMean(mags)));
-		
-		
-		Plot2DPanel plot = new Plot2DPanel();
-		
-		// add a line plot to the PlotPanel
-		plot.addLinePlot("y = x + noise", mags);
-		
-		// put the PlotPanel in a JFrame, as a JPanel
-		JFrame frame = new JFrame("Results");
-		frame.setSize(800, 600);
-		frame.setContentPane(plot);
-		frame.setVisible(true);
-	}
+    // Get 2d array of all data
+    sampleData = dataset.getAllData();
+
+    double[] time = ArrayHelper.extractColumn(sampleData, 0);
+    double[][] sensorData = ArrayHelper.extractColumns(sampleData,
+        new int[] { 1, 2, 3, 4, 5, 6 });
+
+    int steps = CountStepsBlank.countSteps(time, sensorData);
+    System.out.println(steps);
+
+    double[][] accel = ArrayHelper.extractColumns(sampleData,
+        new int[] { 1, 2, 3 });
+    double[] mags = CountStepsBlank.calculateMagnitudesFor(accel);
+
+    System.out.println(CountStepsBlank.calculateMean(mags));
+    System.out.println(CountStepsBlank.calculateStandardDeviation(mags,
+        CountStepsBlank.calculateMean(mags)));
+
+    Plot2DPanel plot = new Plot2DPanel();
+
+    // add a line plot to the PlotPanel
+    plot.addLinePlot("y = x + noise", mags);
+
+    // put the PlotPanel in a JFrame, as a JPanel
+    JFrame frame = new JFrame("Results");
+    frame.setSize(800, 600);
+    frame.setContentPane(plot);
+    frame.setVisible(true);
+  }
 
 }
--- src/CountStepsBlank.java
+++ src/CountStepsBlank.java
@@ -1,98 +1,142 @@
-import java.util.ArrayList;
-
 import javax.swing.JFrame;
+
 import org.math.plot.Plot2DPanel;
 
 public class CountStepsBlank {
 
-	/***
-	 * Counts the number of steps based on sensor data.
-	 * 
-	 * @param times
-	 *            a 1d-array with the elapsed times in miliseconds for each row
-	 *            in the sensorData array.
-	 * 
-	 * @param sensorData
-	 *            a 2d-array where rows represent successive sensor data
-	 *            samples, and the columns represent different sensors. We
-	 *            assume there are 6 columns. Columns 0 - 2 are data from the x,
-	 *            y, and z axes of an accelerometer, and 3-5 are data from the
-	 *            x, y, and z axes of a gyro.
-	 * 
-	 * @return an int representing the number of steps
-	 */
-	private static int countSteps(double[] times, double[][] sensorData) {
-		return 0;
-	}
+  /***
+   * Counts the number of steps based on sensor data.
+   *
+   * @param times
+   *          a 1d-array with the elapsed times in miliseconds for each row in
+   *          the sensorData array.
+   *
+   * @param sensorData
+   *          a 2d-array where rows represent successive sensor data samples,
+   *          and the columns represent different sensors. We assume there are 6
+   *          columns. Columns 0 - 2 are data from the x, y, and z axes of an
+   *          accelerometer, and 3-5 are data from the x, y, and z axes of a
+   *          gyro.
+   *
+   * @return an int representing the number of steps
+   */
+  public static int countSteps(double[] times, double[][] sensorData) {
+    double[] accData = new double[3];
+    int stepCount = 0;
 
-	/***
-	 * Calculate the magnitude for a vector with x, y, and z components.
-	 * 
-	 * @param x
-	 *            the x component
-	 * @param y
-	 *            the y component
-	 * @param z
-	 *            the z component
-	 * @return the magnitude of the vector
-	 */
-	public static double calculateMagnitude(double x, double y, double z) {
-		return Math.sqrt(x*x + y*y + z*z);
-	}
+    // accelerometer data
+    for (int row = 0; row < sensorData.length; row++) {
+      for (int col = 1; col <= 3; col++) {
+        accData = calculateMagnitudesFor(sensorData);
+      }
+    }
 
-	/***
-	 * Takes a 2d array with 3 columns representing the 3 axes of a sensor.
-	 * Calculates the magnitude of the vector represented by each row. Returns a
-	 * new array with the same number of rows where each element contains this
-	 * magnitude.
-	 * 
-	 * @param sensorData
-	 *            an array with n rows and 3 columns. Each row represents a
-	 *            different measurement, and each column represents a different
-	 *            axis of the sensor.
-	 * 
-	 * @return an array with n rows and each element is the magnitude of the
-	 *         vector for the corresponding row in the sensorData array
-	 */
-	public static double[] calculateMagnitudesFor(double[][] sensorData) {
-		double[] mags = new double[sensorData.length];
-		
-		for (int r = 0; r < mags.length; r++) {
-			mags[r] = calculateMagnitude(sensorData[r][0], sensorData[r][1], sensorData[r][2]);
-		}
-		
-		return mags;
-	}
+    double mean = calculateMean(accData);
 
-	/***
-	 * Return the standard deviation of the data.
-	 * 
-	 * @param arr
-	 *            the array of the data
-	 * @param mean
-	 *            the mean of the data (must be pre-calculated).
-	 * @return the standard deviation of the data.
-	 */
-	private static double calculateStandardDeviation(double[] arr, double mean) {
-		return 0.0;
-	}
+    double accStandardDev = calculateStandardDeviation(accData, mean);
 
-	/***
-	 * Return the mean of the data in the array
-	 * 
-	 * @param arr
-	 *            the array of values
-	 * @return the mean of the data
-	 */
-	private static double calculateMean(double[] arr) {
-		return 0.0;
-	}
+    for (int i = 1; i < accData.length - 1; i++) {
+      if (accData[i] > accData[i - 1] && accData[i] > accData[i + 1]) {
+        double threshold = accStandardDev + 14;
 
-	public static void displayJFrame(Plot2DPanel plot) {
-		JFrame frame = new JFrame("Results");
-		frame.setSize(800, 600);
-		frame.setContentPane(plot);
-		frame.setVisible(true);
-	}
+        if (accData[i] > threshold) {
+          stepCount++;
+        } else {
 
+          double percentOfCloseness = (accData[i]) / (threshold);
+          percentOfCloseness = percentOfCloseness * 100;
+
+          if (100 - percentOfCloseness <= 1) {
+            stepCount++;
+            threshold = threshold - (100 - percentOfCloseness);
+          }
+
+          // if accData[i] is below but close to threshold
+          // step++
+        }
+      }
+    }
+    return stepCount;
+  }
+
+  /***
+   * Calculate the magnitude for a vector with x, y, and z components.
+   *
+   * @param x
+   *          the x component
+   * @param y
+   *          the y component
+   * @param z
+   *          the z component
+   * @return the magnitude of the vector
+   */
+  public static double calculateMagnitude(double x, double y, double z) {
+    return Math.sqrt(x * x + y * y + z * z);
+  }
+
+  /***
+   * Takes a 2d array with 3 columns representing the 3 axes of a sensor.
+   * Calculates the magnitude of the vector represented by each row. Returns a
+   * new array with the same number of rows where each element contains this
+   * magnitude.
+   *
+   * @param sensorData
+   *          an array with n rows and 3 columns. Each row represents a
+   *          different measurement, and each column represents a different axis
+   *          of the sensor.
+   *
+   * @return an array with n rows and each element is the magnitude of the
+   *         vector for the corresponding row in the sensorData array
+   */
+  public static double[] calculateMagnitudesFor(double[][] sensorData) {
+    double[] mags = new double[sensorData.length];
+
+    for (int r = 0; r < mags.length; r++) {
+      mags[r] = calculateMagnitude(sensorData[r][0], sensorData[r][1],
+          sensorData[r][2]);
+    }
+
+    return mags;
+  }
+
+  /***
+   * Return the standard deviation of the data.
+   *
+   * @param arr
+   *          the array of the data
+   * @param mean
+   *          the mean of the data (must be pre-calculated).
+   * @return the standard deviation of the data.
+   */
+  public static double calculateStandardDeviation(double[] arr, double mean) {
+    double sum = 0;
+    for (int i = 0; i < arr.length; i++) {
+      double diff = Math.pow((arr[i] - mean), 2);
+      sum += diff;
+    }
+    double num = sum / (arr.length - 1);
+    return Math.sqrt(num);
+  }
+
+  /***
+   * Return the mean of the data in the array
+   *
+   * @param arr
+   *          the array of values
+   * @return the mean of the data
+   */
+  public static double calculateMean(double[] arr) {
+    double sum = 0;
+    for (int i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum / arr.length;
+  }
+
+  public static void displayJFrame(Plot2DPanel plot) {
+    JFrame frame = new JFrame("Results");
+    frame.setSize(800, 600);
+    frame.setContentPane(plot);
+    frame.setVisible(true);
+  }
 }
